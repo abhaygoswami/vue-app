fef2b061549fab778760f7eb2373f65a8ff31e11
abhay676 <abhaygoswami676@gmail.com>
2018-10-19 00:54:44 +0530

    'first-commit'

---
+++ app.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+	el: '#app',
+	data: {
+		header: 'Shopping App',
+		newItem: '',
+		items: [
+			'Buy some coffee',
+			'Antivirus Subscription',
+			'New mobile phone'
+		]
+	}
+});
